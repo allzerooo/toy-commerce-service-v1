fc6a592e71f4c307db1898e49a60c2092c51755f
allzerooo <dychoi.dev@gmail.com>
2025-11-20 10:32:50 +0900

    api-gateway-reactive 모듈 추가

---
+++ api-gateway-reactive/src/main/kotlin/com/toy/apigatewayreactive/ApiGatewayReactiveApplication.kt
@@ -0,0 +1,11 @@
+package com.toy.apigatewayreactive
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiGatewayReactiveApplication
+
+fun main(args: Array<String>) {
+    runApplication<ApiGatewayReactiveApplication>(*args)
+}
---
+++ api-gateway-reactive/src/test/kotlin/com/toy/apigatewayreactive/ApiGatewayReactiveApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.toy.apigatewayreactive
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiGatewayReactiveApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
